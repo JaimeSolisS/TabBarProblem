5a941b715200e6eab66cd3a4ff8f39c52da378f1
Jaime Solís <jaimesolissoto@me.com>
2019-07-02 14:24:26 -0500

    Initial Commit

---
+++ TabBarProblem/LogInViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LogInViewController.swift
+//  TabBarProblem
+//
+//  Created by Jaime Solís on 7/2/19.
+//  Copyright © 2019 ITESM. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func LogIn(_ sender: UIButton) {
+       //Auth.auth...
+        self.performSegue(withIdentifier: "toTabBar", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TabBarProblem/TabBarViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TabBarViewController.swift
+//  TabBarProblem
+//
+//  Created by Jaime Solís on 7/2/19.
+//  Copyright © 2019 ITESM. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    Conectar Botón para
+     
+     @IBAction func LogOut{
+     Auth.auth().signOut()navigationController?.popToRootViewController(animated: true)
+     }
+     
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
